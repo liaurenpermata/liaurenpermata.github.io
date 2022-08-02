72bc1b1b635f59b86ff3c81610e0e4281eb122e0
Liauren Permata Sari <liaurenpermata@gmail.com>
2022-08-02 19:53:25 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// const text = document.querySelector("#profile-name");
+// const strText = text.textContent;
+// const splitText = strText.split("");
+
+// text.textContent = "";
+
+// for(let i=0; i<splitText.length; i++){
+//     text.innerHTML += "<span>" + splitText[i] + "</span>"
+//     console.log(splitText[i]);
+// }
+
+// let char = 0;
+// let timer = setInterval(onTick, 50);
+
+// function onTick(){
+//     const span = text.querySelectorAll('span')[char];
+//     span.classList.add('fade');
+//     char++;
+//     if(char === splitText.length){
+//         complete();
+//         return;
+//     }
+// }
+
+// function complete() {
+//     clearInterval(timer);
+//     timer = null;
+// }
